9bfe604887512c5e5993a51563c3b839d998e678
bandirajesh <bandirajesh209@gmail.com>
2026-07-12 18:56:04 +0530

    change pom.xml

--- server/src/main/java/org/tunnelflow/tunnelflowserver/TunnelFlowServerApplication.java
+++ server/src/main/java/org/tunnelflow/tunnelflowserver/TunnelFlowServerApplication.java
@@ -1,7 +1,10 @@
 package org.tunnelflow.tunnelflowserver;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 @SpringBootApplication
 public class TunnelFlowServerApplication {
@@ -9,5 +12,10 @@ public class TunnelFlowServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(TunnelFlowServerApplication.class, args);
     }
-
+    @Bean
+    CommandLineRunner mappings(RequestMappingHandlerMapping mapping) {
+        return args -> mapping.getHandlerMethods()
+                .forEach((info, method) ->
+                        System.out.println(info + " -> " + method));
+    }
 }
