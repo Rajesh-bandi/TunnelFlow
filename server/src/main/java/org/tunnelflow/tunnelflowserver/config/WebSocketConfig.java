4662354035c6e53fce9f3ac81b1ee5815b9f0813
bandirajesh <bandirajesh209@gmail.com>
2026-07-12 18:23:04 +0530

    logs added to websocket registry

--- server/src/main/java/org/tunnelflow/tunnelflowserver/config/WebSocketConfig.java
+++ server/src/main/java/org/tunnelflow/tunnelflowserver/config/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package org.tunnelflow.tunnelflowserver.config;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
@@ -11,10 +12,12 @@ import org.tunnelflow.tunnelflowserver.websocket.TunnelWebSocketHandler;
 @Configuration
 @EnableWebSocket
 @RequiredArgsConstructor
+@Slf4j
 public class WebSocketConfig implements WebSocketConfigurer {
     private final TunnelWebSocketHandler tunnelWebSocketHandler;
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        log.info("Registering WebSocket endpoint: /ws");
         registry.addHandler(tunnelWebSocketHandler, "/ws")
                 .setAllowedOrigins("*");
     }
